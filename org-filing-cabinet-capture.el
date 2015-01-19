20b35375f1af89840a81641a31a7fdfead45707a
Tom Hartman <thomas.lees.hartman@gmail.com>
2015-01-18 23:32:39 -0500

    Scan capture
    Finished up the bulk of the scan caputring save the middling
    bits. Mostly works.

--- org-filing-cabinet-capture.el
+++ org-filing-cabinet-capture.el
@@ -5,7 +5,12 @@
 (require 'org-filing-cabinet-scan)
 (require 'org-filing-cabinet-auto-commit)
 
-(defcustom org-fc/org-file "~/filing-cabinet/filing-cabinet.org"
+(defcustom org-fc/filing-cabinet-directory "~/filing-cabinet"
+  "Path to filing cabinet."
+  :group 'org-fc
+  :type 'string)
+
+(defcustom org-fc/org-file "filing-cabinet.org"
   "Name of the filing cabinet org file."
   :group 'org-fc
   :type 'string)
@@ -41,6 +46,29 @@
   "Get the capture template."
   (replace-regexp-in-string (regexp-quote "*WHOSE*") org-fc/whose
                             org-fc/capture-template-base))
+
+(defun org-fc/capture-scan-file (file-name)
+  "Scan and capture FILE-NAME into the filing cabinet."
+  (interactive "sName of file: ")
+  (let* ((file-path (org-fc/scan-file file-name))
+         (filing-cabinet-path (f-join (org-fc/current-filing-cabinet-dir)
+                              (f-filename file-path))))
+    (f-move file-path filing-cabinet-path)
+    (org-fc/capture-file filing-cabinet-path)))
+
+(defun org-fc/current-filing-cabinet-dir ()
+  "Return the name of the directory of the current filing cabinet.
+The current filing cabinet is defined as YYYY-mm off the root directory
+of org-fc/filing-cabinet-directory.
+
+If the directory does not exist create it."
+  (let ((retval (f-join org-fc/filing-cabinet-directory
+                        (format-time-string "%Y-%m"))))
+    (when (not (f-directory? retval))
+      (message "Filing cabinet directory `%s' does not exists, creating."
+               retval)
+      (f-mkdir retval))
+    retval))
 
 (provide 'org-filing-cabinet-capture)
 ;;; org-filing-cabinet-capture.el ends here
